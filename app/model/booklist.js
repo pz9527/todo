5d61ce769d7a37fa7da98fb73ca32c308e734eff
“pz9527” <panzhong9527@gmail.com>
2018-08-28 18:38:08 +0800

    model

--- app/model/booklist.js
+++ app/model/booklist.js
@@ -2,7 +2,19 @@ module.exports=app=>{
     const mongoose=app.mongoose;
     const Schema=mongoose.Schema
     const booklistSchema=new Schema({
-        bookId:[{type:Schema.Types.ObjectId,ref:'Book'}],
+        book:[{
+            bookId:{type:Schema.Types.ObjectId,ref:'Book'},
+            score:{type:Number,require:true},
+            reason:String,
+            pushAt:Date,
+            favs:[{type:Schema.Types.ObjectId,ref:'User'}],
+            reply:[{
+                createrId:{type:Schema.Types.ObjectId,ref:'User'},
+                content:String,
+                creatAt:{type:Date,default:Date.now()},
+            }]
+            // hate:[{trpe:Schema.Types.ObjectId,ref:'User'}]
+        }],
         creater:{type:Schema.Types.ObjectId,ref:'User'},
         creatAt:{type:Date,default:Date.now()},
         title:{type:String,require:true},
